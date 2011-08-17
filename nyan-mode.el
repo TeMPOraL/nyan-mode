bfe0a23da498ff570f9c7347eefeb8d3b04a9a98
Jacek Zlydach <temporal.pl@gmail.com>
2011-08-18 00:14:18 +0100

    Beginning development of a standalone nyan-mode.

---
+++ nyan-mode.el
@@ -0,0 +1,88 @@
+;;; nyan-mode.el --- Nyan Cat shows position in current buffer in mode-line.
+;;;
+;;; Nyanyanyanyanyanyanya!
+;;; 
+;;; Author: Jacek "TeMPOraL" Zlydach <temporal.pl@gmail.com>
+;;; URL: http://nyan-mode.buildsomethingamazing.com
+;;; Keywords: nyan, cat, lulz, build something amazing
+
+;;; FIXME INSERT GPL
+
+;;; Most of the code here is stolen from/copied from/inspired by
+;;; sml-modeline.el, written by Lennart Borgman
+;;; See: http://bazaar.launchpad.net/~nxhtml/nxhtml/main/annotate/head%3A/util/sml-modeline.el
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; LICENSE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defgroup nyan nil
+  "Customization group for `nyan-mode'."
+  :group 'frames)
+
+;;; CARGO CULT CODE (copied from somewhere else, to be processed)
+(defun nyan-refresh ()
+  "Refresh after option changes if loaded."
+  (when (featurep 'nyan-mode)
+    (when (and (boundp 'nyan-mode)
+               nyan-mode)
+      (nyan-mode -1)
+      (nyan-mode 1))))
+
+(defcustom nyan-animate-nyancat t
+  "Enable animation for Nyan Cat.
+This can be t or nil."
+  :type '(choice (const :tag "Enabled" t)
+                 (const :tag "Disabled" nil))
+  :set (lambda (sym val)
+         (set-default sym val)
+         (nyan-refresh))
+  :group 'nyan)
+
+;;; TODO maybe customize background color and brackets or NyanCat.
+
+;;; TODO some content
+
+;;; 
+;;; CARGO CULT WARNING I have no idea what it does, maybe will figure
+;;; out later ;).
+;;; Ok, does something with Emacs mode-line-position var.
+;;; TODO figure it out.
+(defvar nyan-old-car-mode-line-position nil)
+
+;;;###autoload
+(define-minor-mode nyan-mode
+  "Use NyanCat to show buffer size and position in mode-line.
+You can customize this minor mode, see option `nyan-mode'.
+
+Note: If you turn this mode on then you probably want to turn off
+option `scroll-bar-mode'."
+  :global t
+  :group 'nyan
+  (if nyan-mode
+      (progn
+        (unless nyan-old-car-mode-line-position
+          (setq nyan-old-car-mode-line-position (car mode-line-position)))
+        (setcar mode-line-position '(:eval (list (nyan-create)))))
+    (setcar mode-line-position nyan-old-car-mode-line-position)))
+
+
+(provide 'nyan)
