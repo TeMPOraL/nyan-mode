561fb059f56a53229deba33767d538fec017a5f6
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2016-04-26 02:37:15 +0200

    Finalizing merging in click-to-scroll.
    + updated readme with another quote.
    + updated source comments with feature announcement.

--- nyan-mode.el
+++ nyan-mode.el
@@ -28,6 +28,9 @@
 
 ;;; Commentary:
 
+;; NEW! You can now click on the rainbow (or the empty space)
+;; to scroll your buffer!
+
 ;; NEW! You can now customize the minimum window width
 ;; below which the nyan-mode will be disabled, so that more important
 ;; information can be shown in the modeline.
@@ -41,7 +44,7 @@
 
 ;;; History:
 
-;; 2016-04-23 - introduced click-to-scroll feature.
+;; 2016-04-26 - introduced click-to-scroll feature.
 
 ;; Started as a totally random idea back in August 2011.
 
