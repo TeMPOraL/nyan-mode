11ea5f24a82f006d5a843ebabcb24fa9d58a673c
Ran Wang <wang1hf@uwindsor.ca>
2021-03-13 15:28:12 -0500

    [definition fix] using more accurate description to tell a node

--- old-count-today-effort.sh
+++ old-count-today-effort.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd ~/Dropbox/myNote/zk/ && git diff ~/Dropbox/myNote/zk/ > ~/.emacs.d/nyan-mode/tmp.log 2>&1
-
-grep '+ *:ID:       ' ~/.emacs.d/nyan-mode/tmp.log | wc -l > ~/.emacs.d/nyan-mode/log.today_effort 2>&1
+grep ':ID:       ' ~/.emacs.d/nyan-mode/tmp.log | wc -l > ~/.emacs.d/nyan-mode/log.today_effort 2>&1
