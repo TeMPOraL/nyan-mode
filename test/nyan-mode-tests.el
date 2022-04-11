4f1274932d77e9b9d06ca80aa9a33a831241f3cf
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2022-04-11 02:15:42 +0200

    Stub tests file
    Nyan Mode will soon have ERT tests. This makes place for them.

---
+++ test/nyan-mode-tests.el
@@ -0,0 +1,20 @@
+;;; nyan-mode-tests.el --- Tests for Nyan Mode -*- lexical-binding: t -*-
+
+;; License: GPLv3
+
+;;; Code:
+
+(require 'ert)
+
+(require 'nyan-mode)
+
+;;; Tests
+(ert-deftest nyan:dummy ()
+  (should (= 1 1)))
+
+
+;; TODO more of actual tests!
+
+
+(provide 'nyan-mode-tests)
+;;; nyan-mode-tests.el ends here
